9230c7fb205fe0bc17c6d7a659fe255bf00ae44e
dvnggak <devanggakertawijaya@gmail.com>
2023-10-04 10:03:39 +0700

    Add User Functionality : Insert data to db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -18,5 +19,35 @@ class HomeController extends Controller
         $data = User::get();
 
         return view('index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => \bcrypt($request->password),
+        ]);
+
+        return redirect()->route('user.index');
     }
 }
