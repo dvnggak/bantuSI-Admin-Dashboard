efe4e99bd399be59d00f35787b765f3b082ddc59
dvnggak <devanggakertawijaya@gmail.com>
2023-10-04 16:00:54 +0700

    register functionality

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [LoginController::class, "login"])->name('login');
 Route::post('/login-process', [LoginController::class, "login_process"])->name('login-process');
 Route::get('/logout', [LogoutController::class, "logout"])->name('logout');
-Route::get('/register', [HomeController::class, "register"])->name('register');
+Route::get('/register', [RegisterController::class, "register"])->name('register');
+Route::post('/register-process', [RegisterController::class, "register_process"])->name('register-process');
 
 // Protected Route
 
